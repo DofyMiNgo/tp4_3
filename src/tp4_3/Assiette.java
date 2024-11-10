a17c04288c90f792992bc3c88647c3eeb5478ff3
Fedi <fedybenrhouma@gmail.com>
2024-11-10 12:03:45 -0800

    commit

---
+++ src/tp4_3/Assiette.java
@@ -0,0 +1,12 @@
+package tp4_3;
+
+abstract public class Assiette extends Ustensile  {
+
+	public Assiette(int annee) {
+		super(annee);
+	
+	}
+abstract double calculesurface();
+
+
+}
---
+++ src/tp4_3/Ustensile.java
@@ -0,0 +1,15 @@
+package tp4_3;
+
+abstract public class Ustensile {
+protected int annee ;
+
+public Ustensile(int annee) {
+	this.annee = annee;
+}
+
+@Override
+public String toString() {
+	return "Ustensile [annee=" + annee + "]";
+} 
+public abstract double valeur();
+}
